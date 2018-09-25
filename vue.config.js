8d8c1a6b13ccbf7515f904413afa6f51100eaca7
王捷 <wangjie@wangjiedeMacBook-Air.local>
2018-09-25 17:45:02 +0800

    build fix

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,11 @@
  * @see {@link https://cli.vuejs.org/config/#global-cli-config}
  */
 
+// 导入compression-webpack-plugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css']
+
 module.exports = {
   // 基本路径
   baseUrl: './',
@@ -16,42 +21,31 @@ module.exports = {
   runtimeCompiler: true,
   // webpack配置
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-  chainWebpack: config => {
-    // 删除预加载功能
-    config.plugins.delete('preload-app')
-
-    // 删除预缓存功能
-    config.plugins.delete('prefetch-app')
-
-    // gzip 压缩功能
-    const CompressionWebpackPlugin = require('compression-webpack-plugin')
-    config.plugin('compression-app').use(CompressionWebpackPlugin, [
-      {
-        // asset: '[path].gz[query]',
-        asset: '[path][query]',
-        algorithm: 'gzip',
-        test: new RegExp('\\.(js|css)$'),
-        // // 文件大于 10KB 才压缩
-        // threshold: 10240,
-        // 不管文件多大都压缩
-        threshold: 0,
-        minRatio: 1
-      }
-    ])
+  chainWebpack: () => {},
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 生产环境
+      config.plugins.push(
+          new CompressionWebpackPlugin({
+              asset: '[path].gz[query]',
+              algorithm: 'gzip',
+              test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+              threshold: 10240,
+              minRatio: 0.8
+          })
+      );
+    } else {
+        // 开发环境
+    }
   },
-  configureWebpack: () => {},
   // 生产环境是否生成 sourceMap 文件
-  productionSourceMap: true,
+  productionSourceMap: false,
   // css相关配置
   css: {
-    // 是否使用css分离插件 ExtractTextPlugin
+    // 是否提取 css 文件到单独的文件中
     extract: true,
-    // 开启 CSS source maps?
-    sourceMap: false,
-    // css预设器配置项
-    loaderOptions: {},
-    // 启用 CSS modules for all css / pre-processor files.
-    modules: false
+    // 是否生成 .css.map 文件
+    sourceMap: false
   },
   // use thread-loader for babel & TS in production build
   // enabled by default if the machine has more than 1 cores
@@ -61,6 +55,7 @@ module.exports = {
   pwa: {},
   // webpack-dev-server 相关配置
   devServer: {
+    open: process.platform === 'darwin',
     host: '0.0.0.0',
     port: 8181,
     https: false,
