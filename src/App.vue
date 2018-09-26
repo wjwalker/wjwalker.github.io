b610fda23cd853bd87c5132ce75e13da36e2c543
王捷 <wangjie@wangjiedeMacBook-Air.local>
2018-09-26 10:50:15 +0800

    github 路由搭建

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
+/*
+ * @Author: wjwalker
+ * @module: github个人主页 
+ * @Date: 2018-09-25 18:55:53 
+ * @Last Modified by: wjwalker
+ * @Last Modified time: 2018-09-25 18:58:21
+ */
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'wjwalker_github_home_page',
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
